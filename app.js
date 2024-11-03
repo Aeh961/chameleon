c5635a2f193a23a9607221375414abfc57e7efa3
AEH <117328917+Abdallahelhamawi@users.noreply.github.com>
2024-11-02 23:44:21 -0700

    First commit

---
+++ app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ContactForm from './ContactForm'; // Adjust the path as needed
+
+const App = () => {
+    return (
+        <div>
+            {/* Other components like Header, Services, etc. */}
+            <ContactForm />
+        </div>
+    );
+};
+
+export default App;
